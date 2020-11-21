934b7767430b31751ee2369d2724de1456df3073
vsvipul <vsvipul555@gmail.com>
2020-11-21 20:50:23 +0530

    Add master slave function on ServeR

--- Server.java
+++ Server.java
@@ -1,8 +1,11 @@
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.rmi.registry.LocateRegistry;
+import java.io.IOException;
+import java.rmi.NotBoundException;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
@@ -10,16 +13,45 @@ import java.util.ArrayList;
 public class Server implements Remote{
     HashMap<String, List<String> > topicSubscriberList;
     ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
-    
-    // Create a lock
+
     private boolean isMasterUp() {
+        // Get slave instance from RMI
+        try {  
+            // Get the registry 
+            Registry registry = LocateRegistry.getRegistry(); 
+            // Look up the registry for the remote object 
+            try {
+                Server stub = (Server) registry.lookup("master");
+                return true;
+            } catch(NotBoundException e) {
+                return false;
+            }
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString()); 
+            e.printStackTrace(); 
+        }
         return false;
     }
     private void becomeMaster() {
-
+        try { 
+            // Export the remote object to the stub 
+            Server stub = (Server) UnicastRemoteObject.exportObject(this, 0);  
+            // Bind the remote object (stub) in the registry 
+            Registry registry = LocateRegistry.getRegistry(); 
+            registry.bind("master", stub);  
+        } catch (Exception e) { 
+            System.err.println("Server exception: " + e.toString()); 
+            e.printStackTrace(); 
+        }
     }
     private void startNewSlave() {
-
+        String[] args = new String[] { "java", "Server"};
+        ProcessBuilder pb = new ProcessBuilder(args);
+        try {
+            pb.start();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
     private void subscribeToSlave(String topic, String UUID, String ReqID) {
         // Get slave instance from RMI
@@ -50,13 +82,13 @@ public class Server implements Remote{
         }
     }
     public void lockMaster() {
-        //lock.lock();
+        reentrantReadWriteLock.writeLock().lock();
     }
     public HashMap<String,List<String> > syncWithSlave() {
         return topicSubscriberList;
     }
     public void unlockMaster() {
-        //lock.unlock();
+        reentrantReadWriteLock.writeLock().unlock();
     }
     private void sendToSubscribers(String topic, Data dt, String ReqID) {
         // Send data to all subscribers of topic
